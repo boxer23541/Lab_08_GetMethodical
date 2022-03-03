8a3104c6e94394fec023c7f544d3aed0f1a7afbb
boxer23541 <alexmb2354@gmail.com>
2022-03-03 14:08:36 -0500

    GetUserName

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class DevTest
 {
-
     public static void main(String[] args)
     {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        String firstName = "";
+        String lastName = "";
+        firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+        lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
+        System.out.println("\nYour full name is: " + firstName + " " + lastName);
     }
 }
+
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class SafeInput
 {
-    
+    /**
+     * Gets a response from user
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = "";  // Set this to zero length. Loop runs until it isn’t
+        do
+        {
+            System.out.println("\n" + prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+
+    }
+
 }
